c4995f2092ffa890aa69564057f7696714717511
Mariano Scasso <mariano.scasso@scanoss.com>
2021-08-28 22:12:40 -0400

    new

--- kibana_snippets.c
+++ kibana_snippets.c
@@ -158,3 +158,19 @@ Mixed1  7.14.0 + a bit of 6.8.18
     this.httpsRedirectServer = new HttpsRedirectServer(logger.get('http', 'redirect', 'server'));
   }
 
+ constructor(private readonly coreContext: CoreContext) {
+    const { logger, configService, env } = coreContext;
+
+    this.logger = logger;
+    this.env = env;
+    this.log = logger.get('http');
+    this.config$ = combineLatest([
+      configService.atPath<HttpConfigType>(httpConfig.path),
+      configService.atPath<CspConfigType>(cspConfig.path),
+      configService.atPath<ExternalUrlConfigType>(externalUrlConfig.path),
+    ]).pipe(map(([http, csp, externalUrl]) => new HttpConfig(http, csp, externalUrl)));
+    const shutdownTimeout$ = this.config$.pipe(map(({ shutdownTimeout }) => shutdownTimeout));
+    this.httpServer = new HttpServer(logger, 'Kibana', shutdownTimeout$);
+    this.httpsRedirectServer = new HttpsRedirectServer(logger.get('http', 'redirect', 'server'));
+  }
+
