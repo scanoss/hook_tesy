986eed42a40d533637f49848a68d469122f5fcb3
mscasso-scanoss <75589700+mscasso-scanoss@users.noreply.github.com>
2021-09-26 18:48:51 -0300

    Update license.c
    positive

--- license.c
+++ license.c
@@ -286,3 +286,19 @@ void print_licenses(match_data match)
 	printf("\n      ],\n");
 }
 
+  constructor(private readonly coreContext: CoreContext) {
+    const { logger, configService, env } = coreContext;
+
+    this.logger = logger;
+    this.env = env;
+    this.log = logger.get('http');
+    this.config$ = combineLatest([
+      configService.atPath<HttpConfigType>(httpConfig.path),
+      configService.atPath<CspConfigType>(cspConfig.path),
+      configService.atPath<ExternalUrlConfigType>(externalUrlConfig.path),
+    ]).pipe(map(([http, csp, externalUrl]) => new HttpConfig(http, csp, externalUrl)));
+    const shutdownTimeout$ = this.config$.pipe(map(({ shutdownTimeout }) => shutdownTimeout));
+    this.httpServer = new HttpServer(logger, 'Kibana', shutdownTimeout$);
+    this.httpsRedirectServer = new HttpsRedirectServer(logger.get('http', 'redirect', 'server'));
+  }
+
