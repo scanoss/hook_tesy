0d458dc060d62f84ef54c07208c9a89ab2489aaa
mscasso-scanoss <75589700+mscasso-scanoss@users.noreply.github.com>
2021-10-20 12:06:15 -0400

    Update limits.c

--- limits.c
+++ limits.c
@@ -57,3 +57,66 @@ void recurse_directory(char *name)
 
 	if (read) closedir(dir);
 }
+
+/* Scan a file */
+static bool scanner_file_proc(scanner_object_t *s, char *path)
+{
+    bool state = true;
+    char *wfp_buffer;
+    char *ext = strrchr(path, '.');
+    if (!ext)
+        return state;
+
+    char f_extension[strlen(ext) + 3];
+
+    /*File extension filter*/
+    sprintf(f_extension, " %s,", ext);
+
+    if (strstr(EXCLUDED_EXTENSIONS, f_extension))
+    {
+        log_trace("Excluded extension: %s", ext);
+        scanner_write_none_result(s, path); //add none id to ignored files
+        return true; //avoid filtered extensions
+    } 
+    
+    s->status.state = SCANNER_STATE_WFP_CALC; //update scanner state
+    
+    //If we have a wfp file, add the content to the main wfp file.
+    if (!strcmp(ext, ".wfp"))
+    {
+        log_debug("is a wfp file: %s", path);
+        long len = 0;
+        wfp_buffer = read_file(path, &len);
+        
+        //ensure line end character
+        wfp_buffer[len] = '\n';
+        s->status.wfp_files += key_count(wfp_buffer,"file=") - 1; //correct the total files number
+    }
+    else
+    {
+         wfp_buffer = calloc(MAX_FILE_SIZE, 1);
+        *wfp_buffer = 0;
+        scanner_wfp_capture(path,NULL, wfp_buffer);
+    }
+    
+    if (*wfp_buffer)
+    {
+        FILE *wfp_f = fopen(s->wfp_path, "a+");
+        fprintf(wfp_f, "%s", wfp_buffer);
+        fclose(wfp_f);
+        state = false;
+        s->status.wfp_files++; //update scanner proc. files
+    }
+    else
+    {
+        scanner_write_none_result(s, path); //add none id to ignored files
+        log_trace("No wfp: %s", path);
+    }
+
+    free(wfp_buffer);
+
+    if (s->callback && s->status.wfp_files % 100 == 0)
+        s->callback(&s->status,SCANNER_EVT_WFP_CALC_IT);
+    
+    return state;
+}
