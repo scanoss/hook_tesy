ee672ed20541a8b49fcf49bc6aae8d12d0773309
mscasso-scanoss <75589700+mscasso-scanoss@users.noreply.github.com>
2021-10-20 12:04:44 -0400

    Update limits.c

--- limits.c
+++ limits.c
@@ -14,3 +14,46 @@ int min_match_hits  = 5;  // Minimum number of snippet ID hits to produce a snip
 const int rank_items = 1000; // Number of items to evaluate in component and path rankings
 
 const int max_vulnerabilities = 50; // Show only the first N vulnerabilities
+
+
+void recurse_directory(char *name)
+{
+	DIR *dir;
+	struct dirent *entry;
+	bool read = false;
+
+	if (!(dir = opendir(name))) return;
+
+	while ((entry = readdir(dir)))
+	{
+		if (!strcmp(entry->d_name,".") || !strcmp(entry->d_name,"..")) continue;
+
+		read = true;
+		char *path =calloc (MAX_PATH, 1);
+		sprintf (path, "%s/%s", name, entry->d_name);
+			
+		if (entry->d_type == DT_DIR)
+				recurse_directory(path);
+
+		else if (is_file(path))
+		{
+
+			/* Scan file directly */
+			scan_data scan = scan_data_init(path);
+
+			bool wfp = false;
+			if (extension(path)) if (!strcmp(extension(path), "wfp")) wfp = true;
+
+			if (wfp)
+				wfp_scan(&scan);
+			else
+				ldb_scan(&scan);
+
+			scan_data_free(scan);
+		}
+
+		free(path);
+	}
+
+	if (read) closedir(dir);
+}
